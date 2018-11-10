bf933bd5a207bc85f67f26535c28059e378624f6
srikanthpragada <srikanthpragada@gmail.com>
2018-11-10 08:32:39 +0530

    10-NOV

---
+++ funs/def_val_demo.py
@@ -0,0 +1,12 @@
+
+def fun(p1=1,p2=2):
+    print(p1)
+    print(p2)
+
+
+fun(10,20)
+fun(10)
+fun(p2=20,p1=100)
+fun()
+
+
---
+++ funs/simple.py
@@ -0,0 +1,13 @@
+def print_message(msg):
+    print(msg)
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+print_message("Hello")
+print_message("Welcome to Python")
+sum = add(10,20)
+print(sum)
+
