d0f20e7e82bf7468518861c6d7a87dc0400cf723
srikanthpragada <srikanthpragada@gmail.com>
2018-11-19 08:31:39 +0530

    19-NOV

---
+++ oop/ex_demo.py
@@ -0,0 +1,14 @@
+try:
+    num = int(input("Enter a number :"))
+    result = 100 / num
+except ZeroDivisionError:
+    print("Sorry! Number cannot be 0")
+except Exception  as ex:
+     print("Error : ", ex)
+else:
+    print("Result = ",result)
+finally:
+    print("** Completed **")
+
+
+print("*** The  End ***")
